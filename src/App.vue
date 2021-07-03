12989458d9df54371a8af18d774843ea47450e69
Dominic Law <dominic.law79@gmail.com>
2021-07-03 13:28:30 +1000

    added basic nav + header wip

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="banner">
-    <img class="stretch" src="./assets/header.jpg"/>
-    <!-- <div class="overlay">
-      <img class="stretch" src="./assets/header.jpg"/>
-    </div> -->
-  </div>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-transparent ">
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto px-5">
-        <li class="nav-item active px-4">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item px-4">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item px-4">
-          <a class="nav-link" href="#">Projects</a>
-        </li>
-        <li class="nav-item px-4">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
+  <div id="wrapper">
+    <div class="banner selectDisable">
+      <img class="stretch" draggable="false" src="./assets/header.jpg"/>
     </div>
-  </nav>
-  <div class="container">
-      <h1>Hello World</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent ">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto px-5">
+          <li class="nav-item active px-4">
+            <a class="nav-link" href="#">HOME</a>
+          </li>
+          <li class="nav-item px-4">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item px-4">
+            <a class="nav-link" href="#">Projects</a>
+          </li>
+          <li class="nav-item px-4">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div id="header">
+        <p class="typewriterAnim-1 line-1">My name is Dominic Law.<br /></p>
+        <!-- <p class="typewriterAnim-2 line-2">I am a 4th year student actively<br /></p> -->
+        <!-- <p class="typewriterAnim line">pursuing a career in the<br /></p>
+        <p class="typewriterAnim line">Software Development landscape</p> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -46,11 +50,18 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+#wrapper {
+  padding: 0 15vh;
+}
+
+/* Navigation */
 .navbar {
   margin-top: 50px;
   text-align: right;
   background: none;
 }
+
+/* Header */
 .banner {
   width: 100%; 
   height: 100%; 
@@ -60,9 +71,7 @@ export default {
   z-index: -1;
   opacity: 90%;
 }
-.overlay {
-  background-color: black;
-}
+
 .banner img {
   filter: brightness(20%)
 }
@@ -70,4 +79,74 @@ export default {
   width:100%;
   height:100%;
 }
+#header {
+  text-align: left;
+  color: #EDEDED;
+  font-family: 'Anonymous Pro', monospace;
+  background-color: rgb(25,25,25);
+}
+.line-1 {
+  position: relative;
+  border-right: 2px solid rgba(255, 255, 255, 0.75);
+  overflow: hidden; transform: translateY(-50%);
+  width: 20em;
+  margin: 0 auto;
+  white-space: nowrap;
+}
+
+.line-2 {
+  position: relative;
+  border-right: 2px solid rgba(255, 255, 255, 0.75);
+  overflow: hidden; transform: translateY(-50%);
+  width: 20em;
+  margin: 0 auto;
+  white-space: nowrap;
+}
+
+.typewriterAnim-1 {
+  animation: typewriter-1 4s steps(40) 1s 1 normal both,
+  blinkTextCursor 750ms steps(40) infinite normal;
+}
+.typewriterAnim-2 {
+  animation: typewriter-2 4s steps(40) 5s 1 normal both,
+  blinkTextCursor 750ms steps(40) infinite normal;
+}
+.typewriterAnim-3 {
+  animation: typewriter 4s steps(40) 9s 1 normal both,
+  blinkTextCursor 750ms steps(40) infinite normal;
+}
+.typewriterAnim-4 {
+  animation: typewriter 4s steps(40) 13s 1 normal both,
+  blinkTextCursor 750ms steps(40) infinite normal;
+}
+
+@keyframes typewriter-1 {
+  from {
+    width: 0;
+  }
+  to {
+    width: 13em;
+  }
+}
+
+@keyframes typewriter-2 {
+  from {
+    width: 0;
+  }
+  to {
+    width: 18em;
+  }
+}
+
+@keyframes blinkTextCursor {
+  from {
+    border-rigght-color: rgba(255, 255, 255, 0.75);
+  }
+  to {
+    border-right-color: transparent;
+  }
+}
+/* Misc styling */
+
+
 </style>
