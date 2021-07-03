cac2359cae99ba859da410d3aa1f4074f1ff0aae
Dominic Law <dominic.law79@gmail.com>
2021-07-03 17:27:55 +1000

    added header animation + highlighting

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="wrapper">
-    <div class="banner selectDisable">
-      <img class="stretch" draggable="false" src="./assets/header.jpg"/>
-    </div>
+    <div class="banner"/>
     <nav class="navbar navbar-expand-lg navbar-dark bg-transparent ">
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto px-5">
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item active px-4">
             <a class="nav-link" href="#">HOME</a>
           </li>
@@ -15,20 +13,18 @@
           <li class="nav-item px-4">
             <a class="nav-link" href="#">Projects</a>
           </li>
-          <li class="nav-item px-4">
+          <li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
           </li>
         </ul>
       </div>
     </nav>
-    <div class="container">
-      <div id="header">
-        <p class="typewriterAnim-1 line-1">My name is Dominic Law.<br /></p>
-        <!-- <p class="typewriterAnim-2 line-2">I am a 4th year student actively<br /></p> -->
-        <!-- <p class="typewriterAnim line">pursuing a career in the<br /></p>
-        <p class="typewriterAnim line">Software Development landscape</p> -->
-      </div>
-    </div>
+    <header id="header-content">
+      <p>My name is <span class="highlight">Dominic Law</span>.<br /></p>
+      <p>I am currently in my <span class="highlight">final year</span> at Macquarie University<br /></p>
+      <p>actively pursuing a career in the <span class="highlight">Software Development</span> <br /></p>
+      <p>landscape and aiming to perfect my craft.</p>
+    </header>
   </div>
 </template>
 
@@ -51,7 +47,7 @@ export default {
   color: #2c3e50;
 }
 #wrapper {
-  padding: 0 15vh;
+  padding: 0 25vh;
 }
 
 /* Navigation */
@@ -63,89 +59,148 @@ export default {
 
 /* Header */
 .banner {
+  background-image: url('./assets/header.jpg');
+  background-size: cover;
+  background-position: cover;
+  height: 100vh;
   width: 100%; 
-  height: 100%; 
   position: fixed; 
+  display: flex;
+  flex-direction: column;
   left: 0px; 
   top: 0px; 
-  z-index: -1;
   opacity: 90%;
+  filter: brightness(20%);
+  z-index: -1;
 }
 
-.banner img {
-  filter: brightness(20%)
-}
-.stretch {
+
+/* Header + Animations */
+#header-content {
   width:100%;
-  height:100%;
+  height: 100%;
+  margin-top: 25%;
 }
-#header {
+
+#header-content p {
+  vertical-align: center;
   text-align: left;
   color: #EDEDED;
   font-family: 'Anonymous Pro', monospace;
-  background-color: rgb(25,25,25);
-}
-.line-1 {
-  position: relative;
-  border-right: 2px solid rgba(255, 255, 255, 0.75);
-  overflow: hidden; transform: translateY(-50%);
-  width: 20em;
-  margin: 0 auto;
   white-space: nowrap;
+  overflow: hidden;
+  border-right: .15em solid white;
 }
 
-.line-2 {
-  position: relative;
-  border-right: 2px solid rgba(255, 255, 255, 0.75);
-  overflow: hidden; transform: translateY(-50%);
-  width: 20em;
-  margin: 0 auto;
-  white-space: nowrap;
+#header-content p:nth-child(1) {
+  width: 13em;
+  -webkit-animation: type 3s steps(40, end);
+  animation: type 3s steps(40, end);
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
 }
 
-.typewriterAnim-1 {
-  animation: typewriter-1 4s steps(40) 1s 1 normal both,
-  blinkTextCursor 750ms steps(40) infinite normal;
-}
-.typewriterAnim-2 {
-  animation: typewriter-2 4s steps(40) 5s 1 normal both,
-  blinkTextCursor 750ms steps(40) infinite normal;
-}
-.typewriterAnim-3 {
-  animation: typewriter 4s steps(40) 9s 1 normal both,
-  blinkTextCursor 750ms steps(40) infinite normal;
-}
-.typewriterAnim-4 {
-  animation: typewriter 4s steps(40) 13s 1 normal both,
-  blinkTextCursor 750ms steps(40) infinite normal;
+#header-content p:nth-child(2) {
+  width: 30.5em;
+  opacity: 0;
+  -webkit-animation: type 3s steps(40, end);
+  animation: type 3s steps(40, end);
+  -webkit-animation-delay:3s;
+  animation-delay: 3s;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
 }
 
-@keyframes typewriter-1 {
-  from {
+#header-content p:nth-child(3) {
+  width: 30em;
+  opacity: 0;
+  -webkit-animation: type 3s steps(40, end);
+  animation: type 3s steps(40, end);
+  -webkit-animation-delay: 6s;
+  animation-delay: 6s;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+#header-content p:nth-child(4) {
+  width: 22.5em;
+  opacity: 0;
+  -webkit-animation: type2 3s steps(40, end), blink .5s step-end infinite alternate;
+  animation: type2 3s steps(40, end), blink .5s step-end infinite alternate;
+  -webkit-animation-delay: 9s;
+  animation-delay: 9s;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+@keyframes type {
+  0% {
     width: 0;
   }
-  to {
-    width: 13em;
+  1% {
+    opacity: 1;
+  }
+  99.9% {
+    border-right: .15em solid white;
+  }
+  100% {
+    opacity: 1;
+    border: none;
   }
 }
 
-@keyframes typewriter-2 {
-  from {
+@-webkit-keyframes type {
+  0% {
     width: 0;
   }
-  to {
-    width: 18em;
+  1% {
+    opacity: 1;
+  }
+  99.9% {
+    border-right: .15em solid white;
+  }
+  100% {
+    opacity: 1;
+    border: none;
   }
 }
 
-@keyframes blinkTextCursor {
-  from {
-    border-rigght-color: rgba(255, 255, 255, 0.75);
+@keyframes type2 {
+  0% {
+    width: 0;
   }
-  to {
-    border-right-color: transparent;
+  1% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
   }
 }
+
+@keyframes blink {
+  50% {
+    border-color: transparent;
+  }
+}
+@-webkit-keyframes blink {
+  50% {
+    border-color: tranparent;
+  }
+}
+
+/* Header highlighting */
+.highlight {
+  background-color: #651e3e;
+  background-position: 0% 0%;
+  background-size: 100% 100%;
+  transition: background-position .5s, background-size .3s ease-in 3s;
+}
+
+#about {
+  display: block;
+  background-color: black;
+}
+
 /* Misc styling */
 
 
