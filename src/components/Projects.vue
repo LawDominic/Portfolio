79741abdd8a65e075ea59df384367cd120ecdac1
Dominic Law <dominic.law79@gmail.com>
2021-07-07 21:36:40 +1000

    Cards WIP

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,16 @@
 <template>
   <section id="projects">
     <h1 class="secHeading display-5">Some things I've worked on</h1>
-    <p id="projects-content"></p>
+    <p>WIP</p>
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</p>
+            </div>
+            <div class="col-sm-3"><img src="https://picsum.photos/500/501"/></div>
+        </div>
+    </div> -->
   </section>
 </template>
 
@@ -12,6 +21,33 @@ export default {
 </script>
 
 <style scoped>
+
+.row {
+    background-color: white;
+    border-radius: 10px;
+    text-align: left;
+    height: 300px;
+}
+
+.card-body {
+    padding: 40px;
+}
+
+.card-title {
+    font-weight: bold;
+}
+
+.card-text {
+    margin-top: 20px;
+    font-weight: normal;
+}
+
+.col-sm-12 {
+    padding: 0;
+    float: right;
+    height: 300px;
+}
+
 #projects {
   background-color: #ededed;
   width: 100%;
@@ -25,6 +61,7 @@ export default {
     font-size: 10px;
   }
 }
+
 /* Small devices (landscape phones, 576px and up) */
 @media (min-width: 576px) {
   #projects-content {
