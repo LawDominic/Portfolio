54f23860b6ed621dc9b466eab439bc10d3862b6c
Dominic Law <dominic.law79@gmail.com>
2021-07-11 14:56:50 +1000

    update

--- src/App.vue
+++ src/App.vue
@@ -85,7 +85,7 @@ export default {
 
 /* Header */
 .banner {
-  background-image: url("/assets/header.jpg");
+  background-image: url("/assets/header.33fd60dc.jpg");
   background-size: cover;
   background-position: cover;
   height: 100vh;
